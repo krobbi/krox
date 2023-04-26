1ec53f177f9528fba2233e8b31c8ab61deb07c4f
Chris Roberts <70552146+krobbi@users.noreply.github.com>
2023-04-26 23:30:57 +0100

    Chapter 4

--- src/krox.py
+++ src/krox.py
@@ -2,15 +2,65 @@
 
 import sys
 
+from krox_error_reporter import ErrorReporter
+from krox_scanner import Scanner
+from krox_token import Token
 from typing import Self
 
 class Krox:
     """ Java-style main class to match the JLox implementation. """
     
+    error_reporter: ErrorReporter
+    """ The error reporter to pass through the interpreter. """
+    
+    def __init__(self: Self) -> None:
+        """ Initialize Krox. """
+        
+        self.error_reporter = ErrorReporter()
+    
+    
     def main(self: Self, args: list[str]) -> None:
         """ Run Krox from arguments. """
         
-        print("Hello, Krox!")
+        if len(args) > 1:
+            print("Usage: krox.py [script]")
+            sys.exit(64)
+        elif len(args) == 1:
+            self.run_file(args[0])
+        else:
+            self.run_prompt()
+    
+    
+    def run_file(self: Self, path: str) -> None:
+        """ Run Krox from a file path. """
+        
+        with open(path) as file:
+            self.run(file.read())
+            
+            if self.error_reporter.had_error():
+                sys.exit(65)
+    
+    
+    def run_prompt(self: Self) -> None:
+        """ Run Krox from a prompt. """
+        
+        while True:
+            try:
+                line: str = input("> ")
+                self.run(line)
+                self.error_reporter.clear()
+            except EOFError:
+                break
+    
+    
+    def run(self: Self, source: str) -> None:
+        """ Run Krox from source code. """
+        
+        scanner: Scanner = Scanner(self.error_reporter, source)
+        tokens: list[Token] = scanner.scan_tokens()
+        
+        for token in tokens:
+            print(token)
 
 
 if __name__ == "__main__":
---
+++ src/krox_token_type.py
@@ -0,0 +1,121 @@
+from enum import Enum, auto
+
+class TokenType(Enum):
+    """ The type of a token. """
+    
+    LEFT_PAREN = auto()
+    """ `(`. """
+    
+    RIGHT_PAREN = auto()
+    """ `)`. """
+    
+    LEFT_BRACE = auto()
+    """ `{`. """
+    
+    RIGHT_BRACE = auto()
+    """ `}`. """
+    
+    COMMA = auto()
+    """ `,`. """
+    
+    DOT = auto()
+    """ `.`. """
+    
+    MINUS = auto()
+    """ `-`. """
+    
+    PLUS = auto()
+    """ `+`. """
+    
+    SEMICOLON = auto()
+    """ `;`. """
+    
+    SLASH = auto()
+    """ `/`. """
+    
+    STAR = auto()
+    """ `*`. """
+    
+    BANG = auto()
+    """ `!`. """
+    
+    BANG_EQUAL = auto()
+    """ `!=`. """
+    
+    EQUAL = auto()
+    """ `=`. """
+    
+    EQUAL_EQUAL = auto()
+    """ `==`. """
+    
+    GREATER = auto()
+    """ `>`. """
+    
+    GREATER_EQUAL = auto()
+    """ `>=`. """
+    
+    LESS = auto()
+    """ `<`. """
+    
+    LESS_EQUAL = auto()
+    """ `<=`. """
+    
+    IDENTIFIER = auto()
+    """ Identifier literal. """
+    
+    STRING = auto()
+    """ String literal. """
+    
+    NUMBER = auto()
+    """ Number literal. """
+    
+    AND = auto()
+    """ `and`. """
+    
+    CLASS = auto()
+    """ `class`. """
+    
+    ELSE = auto()
+    """ `else`. """
+    
+    FALSE = auto()
+    """ `false`. """
+    
+    FUN = auto()
+    """ `fun`. """
+    
+    FOR = auto()
+    """ `for`. """
+    
+    IF = auto()
+    """ `if`. """
+    
+    NIL = auto()
+    """ `nil`. """
+    
+    OR = auto()
+    """ `or`. """
+    
+    PRINT = auto()
+    """ `print`. """
+    
+    RETURN = auto()
+    """ `return`. """
+    
+    SUPER = auto()
+    """ `super`. """
+    
+    THIS = auto()
+    """ `this`. """
+    
+    TRUE = auto()
+    """ `true`. """
+    
+    VAR = auto()
+    """ `var`. """
+    
+    WHILE = auto()
+    """ `while`. """
+    
+    EOF = auto()
+    """ End of file marker. """
