288e48d27264b6c51cfb7fd97244bc5bba5c2dc8
Chris Roberts <70552146+krobbi@users.noreply.github.com>
2023-05-03 16:34:10 +0100

    Chapter 15

--- ckrox/common.h
+++ ckrox/common.h
@@ -1,6 +1,8 @@
 #ifndef ckrox_common_h
 #define ckrox_common_h
 
+#define DEBUG_TRACE_EXECUTION
+
 #include <stdbool.h>
 #include <stddef.h>
 #include <stdint.h>
--- ckrox/main.c
+++ ckrox/main.c
@@ -1,19 +1,65 @@
 #include "common.h"
 #include "chunk.h"
 #include "debug.h"
+#include "vm.h"
 
-/* Main function. Test the bytecode disassembler. */
+/* Main function. Test the bytecode disassembler and VM. */
 int main(int argc, char* argv[]) {
+	initVM();
 	Chunk chunk;
 	initChunk(&chunk);
 	
+	/*
+	* -((1.2 + 3.4) / 5.6)
+	*/
+	
+	/*
+	* -((1.2 + 3.4) / 5.6)
+	*    ^^^
+	*/
 	int constant = addConstant(&chunk, 1.2);
 	writeChunk(&chunk, OP_CONSTANT, 123);
 	writeChunk(&chunk, constant, 123);
 	
+	/*
+	* -((1.2 + 3.4) / 5.6)
+	*          ^^^
+	*/
+	constant = addConstant(&chunk, 3.4);
+	writeChunk(&chunk, OP_CONSTANT, 123);
+	writeChunk(&chunk, constant, 123);
+	
+	/*
+	* -((1.2 + 3.4) / 5.6)
+	*    ~~~~^~~~~
+	*/
+	writeChunk(&chunk, OP_ADD, 123);
+	
+	/*
+	* -((1.2 + 3.4) / 5.6)
+	*                 ^^^
+	*/
+	constant = addConstant(&chunk, 5.6);
+	writeChunk(&chunk, OP_CONSTANT, 123);
+	writeChunk(&chunk, constant, 123);
+	
+	/*
+	* -((1.2 + 3.4) / 5.6)
+	*   ~~~~~~~~~~~~^~~~~
+	*/
+	writeChunk(&chunk, OP_DIVIDE, 123);
+	
+	/*
+	* -((1.2 + 3.4) / 5.6)
+	* ^~~~~~~~~~~~~~~~~~~~
+	*/
+	writeChunk(&chunk, OP_NEGATE, 123);
+	
 	writeChunk(&chunk, OP_RETURN, 123);
 	
 	disassembleChunk(&chunk, "test chunk");
+	interpret(&chunk);
+	freeVM();
 	freeChunk(&chunk);
 	return 0;
 }
