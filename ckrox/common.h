ab1b19641cbbd0aa6c655e8b5bfa200d51da5b69
Chris Roberts <70552146+krobbi@users.noreply.github.com>
2023-05-04 16:25:35 +0100

    Chapter 17

--- ckrox/common.h
+++ ckrox/common.h
@@ -1,6 +1,7 @@
 #ifndef ckrox_common_h
 #define ckrox_common_h
 
+#define DEBUG_PRINT_CODE
 #define DEBUG_TRACE_EXECUTION
 
 #include <stdbool.h>
--- ckrox/vm.c
+++ ckrox/vm.c
@@ -98,6 +98,19 @@ static InterpretResult run() {
 
 /* Interpret Krox source code and return an interpret result. */
 InterpretResult interpret(const char* source) {
-	compile(source);
-	return INTERPRET_OK;
+	Chunk chunk;
+	initChunk(&chunk);
+	
+	if (!compile(source, &chunk)) {
+		freeChunk(&chunk);
+		return INTERPRET_COMPILE_ERROR;
+	}
+	
+	vm.chunk = &chunk;
+	vm.ip = vm.chunk->code;
+	
+	InterpretResult result = run();
+	
+	freeChunk(&chunk);
+	return result;
 }
